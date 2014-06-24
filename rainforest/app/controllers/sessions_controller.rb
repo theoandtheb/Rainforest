68bb13f47c002d37151cd175a12c1539e2523218
theoandtheb <blue.apple@sympatico.ca>
2014-06-24 15:39:47 -0400

    Users now have names, and they are referred to when logged in and when displaying reviews.

--- rainforest/app/controllers/sessions_controller.rb
+++ rainforest/app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   	user = User.find_by_email(params[:email])
   	if user && user.authenticate(params[:password])
   		session[:user_id] = user.id
-  		redirect_to products_url, :notice => "You are logged in"
+  		redirect_to products_url
   	else
   		flash.now[:alert] = "Either the email address or password you entered is invalid.  Please try again."
   		render "new"
